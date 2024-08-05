87ad87e45c34287c31007dd25f7380af59c9d983
trantuanthinh <tran.tuan.thinh.0125@gmail.com>
2024-08-05 20:39:12 +0700

    delete ci-cd file

--- Controllers/Auth.Controller.cs
+++ Controllers/Auth.Controller.cs
@@ -1,6 +1,16 @@
-﻿namespace uni_cap_pro_be.Controllers
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using uni_cap_pro_be.Interfaces;
+using uni_cap_pro_be.Utils;
+
+namespace uni_cap_pro_be.Controllers
 {
-	public class Auth
+	[Route("/api/[controller]")]
+	[ApiController]
+	public class Auth(IUserService userService, IMapper mapper, SharedService sharedService)
 	{
+		private readonly IUserService _userService = userService;
+		private readonly IMapper _mapper = mapper;
+		private readonly SharedService _sharedService = sharedService;
 	}
 }
